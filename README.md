c4ffc9a712a43fce80695ed8920aae066ef5b890
Gabriel Madrid <gabriel.madrid@br.experian.com>
2024-03-29 19:48:16 -0300

    Added Flux installation

---
+++ README.md
@@ -0,0 +1,11 @@
+# Startup execution
+Github Personal Access Token(PAT): https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens
+
+```
+./startup.sh -u {github username} -t {github PAT token}
+```
+
+# Cleanup
+```
+./cleanup.sh
+```
--- startup.sh
+++ startup.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
+Help()
+{
+    # Display Help
+    echo "The commands are: "
+    echo
+    echo "u    Github Username to authenticate on repository."
+    echo "t    Github PAT to authenticate on repository, see the docs https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens ."
+    echo
+    echo "Usage: "
+    echo
+    echo "./startup.sh -u <string> -t <string>"
+    echo
+}
+
+while getopts ":u:t:" option; do
+    case $option in
+        h)
+            Help
+            exit;;
+         \?)
+            echo "Error: Invalid option"
+            exit;;
+        u) githubuser=${OPTARG};;
+        t) githubtoken=${OPTARG};;
+    esac
+done
+
+export GITHUB_TOKEN=$githubtoken
+
 echo "Checking binaries requirements."
 requirements=("kind" "helm" "curl" "flux")
 for binary in ${requirements[@]}; do 
-    echo $binary
     if ! command -v $binary &> /dev/null; then
         echo "ERROR - $binary not installed."
         if [ $binary = "flux" ]; then
@@ -21,7 +49,11 @@ kind create cluster --config=clusters/target-apps.yaml
 
 echo -e "\nCreated clusters:"
 kind get clusters
+clusters=$(kind get clusters)
 
-echo -e "\nSetting context to Istio cluster."
-kubectl config use-context kind-istio
+for cluster in ${clusters[@]};do
+    echo -e "\nSetting context to $cluster cluster."
+    kubectl config use-context kind-$cluster
+    flux bootstrap github --token-auth --owner=$githubuser --repository=gitops-istio-multi-cluster --branch=main --path=gitops/$cluster --personal
+done
 
