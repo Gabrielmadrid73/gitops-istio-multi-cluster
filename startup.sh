e1b62fb59dc026fb49167c8a2605b83d2b03947e
gabrielmadrid <gabrielmadrid73@gmail.com>
2024-04-12 01:01:09 +0000

    adjusted to kubeadm

--- startup.sh
+++ startup.sh
@@ -12,7 +12,9 @@ Help()
     echo "./startup.sh -u <string> -t <string>"
     echo
 }
-
+function kconfig () {
+    export KUBECONFIG=~/.kube/$1
+}
 while getopts ":u:t:" option; do
     case $option in
         h)
@@ -29,7 +31,7 @@ done
 export GITHUB_TOKEN=$githubtoken
 
 echo "Checking binaries requirements."
-requirements=("kind" "helm" "curl" "flux" "istioctl")
+requirements=("helm" "curl" "flux" "istioctl")
 for binary in ${requirements[@]}; do 
     if ! command -v $binary &> /dev/null; then
         echo "ERROR - $binary not installed."
@@ -43,19 +45,23 @@ make -f Makefile.selfsigned.mk root-ca
 make -f Makefile.selfsigned.mk istio-cacerts
 
 for cluster in ${clusters_file[@]};do
-    kubectl create namespace istio-system --kubeconfig=~/.kube/$cluster
-    kubectl label namespace istio-system istio-injection=enabled --kubeconfig=~/.kube/$cluster
-    kubectl create secret generic cacerts -n istio-system --from-file=istio/ca-cert.pem --from-file=istio/ca-key.pem --from-file=istio/root-cert.pem --from-file=istio/cert-chain.pem --kubeconfig=~/.kube/$cluster
-    flux bootstrap github --token-auth --owner=$githubuser --repository=gitops-istio-multi-cluster --branch=main --path=gitops/$cluster/flux-resources --personal --kubeconfig=~/.kube/$cluster
+    kconfig $cluster
+    kubectl create namespace istio-system
+    kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+    kubectl taint nodes --all node-role.kubernetes.io/master-
+    kubectl label namespace istio-system istio-injection=enabled
+    kubectl create secret generic cacerts -n istio-system --from-file=istio/ca-cert.pem --from-file=istio/ca-key.pem --from-file=istio/root-cert.pem --from-file=istio/cert-chain.pem
+    flux bootstrap github --token-auth --owner=$githubuser --repository=gitops-istio-multi-cluster --branch=main --path=gitops/$cluster/flux-resources --personal
     # Wait for Istio namespace and installation / SA conflict istio-reader-service-account
     sleep 160
-    istioctl create-remote-secret --name=$cluster --kubeconfig=~/.kube/$cluster > secret-$cluster.yaml
+    istioctl create-remote-secret --name=$cluster > secret-$cluster.yaml
 done
-
-kubectl apply -f secret-istio.yaml --kubeconfig=~/.kube/source-apps
-kubectl apply -f secret-source-apps.yaml --kubeconfig=~/.kube/istio
+kconfig source-apps
+kubectl apply -f secret-istio.yaml
+kubectl rollout restart deploy ecsdemo-crystal ecsdemo-frontend ecsdemo-nodejs -n istio-system
+kconfig istio
+kubectl apply -f secret-source-apps.yaml
+kubectl rollout restart deploy nginx -n istio-system
 
 rm -rf secret-istio.yaml secret-source-apps.yaml
 
-kubectl rollout restart deploy ecsdemo-crystal ecsdemo-frontend ecsdemo-nodejs -n istio-system --kubeconfig=~/.kube/source-apps
-kubectl rollout restart deploy nginx -n istio-system --kubeconfig=~/.kube/istio
