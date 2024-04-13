e990a61b3468fc891cba8d296c43898850eb2768
Gabrielmadrid73 <gabrielmadrid73@gmail.com>
2024-04-12 23:36:07 -0300

    Updated code to operator

--- cleanup-clusters.sh
+++ cleanup-clusters.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -rf istio/ca-cert.pem istio/ca-key.pem istio/root-cert.pem istio/cert-chain.pem root-ca.conf root-cert.csr root-cert.pem root-key.pem root-cert.srl
+rm -rf istio/*.pem source-apps/*.pem root-c*
--- startup.sh
+++ startup.sh
@@ -20,18 +20,23 @@ while getopts ":u:t:" option; do
         h)
             Help
             exit;;
-         \?)
+        \?)
             echo "Error: Invalid option"
             exit;;
-        u) githubuser=${OPTARG};;
-        t) githubtoken=${OPTARG};;
+        u)
+            githubuser=${OPTARG};;
+        t)
+            githubtoken=${OPTARG};;
+        -*)
+            echo "Error: Unsupported flag $1" >&2
+            exit 1;;
     esac
 done
 
 export GITHUB_TOKEN=$githubtoken
 
 echo "Checking binaries requirements."
-requirements=("helm" "curl" "flux" "istioctl")
+requirements=("flux" "istioctl")
 for binary in ${requirements[@]}; do 
     if ! command -v $binary &> /dev/null; then
         echo "ERROR - $binary not installed."
@@ -42,26 +47,31 @@ done
 clusters_file=(istio source-apps)
 
 make -f Makefile.selfsigned.mk root-ca
-make -f Makefile.selfsigned.mk istio-cacerts
+
 
 for cluster in ${clusters_file[@]};do
+    make -f Makefile.selfsigned.mk $cluster-cacerts
     kconfig $cluster
+    ip=$(cat $KUBECONFIG | grep server | awk '{print $2}' | sed 's/:6443//' | sed 's/https:\/\///')
     kubectl create namespace istio-system
-    kubectl taint nodes --all node-role.kubernetes.io/control-plane-
-    kubectl taint nodes --all node-role.kubernetes.io/master-
-    kubectl label namespace istio-system istio-injection=enabled
-    kubectl create secret generic cacerts -n istio-system --from-file=istio/ca-cert.pem --from-file=istio/ca-key.pem --from-file=istio/root-cert.pem --from-file=istio/cert-chain.pem
+    kubectl label namespace istio-system topology.istio.io/network=network-$cluster
+    kubectl create secret generic cacerts -n istio-system --from-file=$cluster/ca-cert.pem --from-file=$cluster/ca-key.pem --from-file=$cluster/root-cert.pem --from-file=$cluster/cert-chain.pem
     flux bootstrap github --token-auth --owner=$githubuser --repository=gitops-istio-multi-cluster --branch=main --path=gitops/$cluster/flux-resources --personal
     # Wait for Istio namespace and installation / SA conflict istio-reader-service-account
-    sleep 160
+    echo "waiting 5 minutes to resources be running..."
+    sleep 300
     istioctl create-remote-secret --name=$cluster > secret-$cluster.yaml
+    kubectl patch service istio-eastwestgateway --patch "{\"spec\": {\"externalIPs\": [${ip}]}}" -n istio-system
 done
+
 kconfig source-apps
 kubectl apply -f secret-istio.yaml
-kubectl rollout restart deploy ecsdemo-crystal ecsdemo-frontend ecsdemo-nodejs -n istio-system
+# kubectl rollout restart deploy ecsdemo-crystal ecsdemo-frontend ecsdemo-nodejs -n apps
+
 kconfig istio
 kubectl apply -f secret-source-apps.yaml
-kubectl rollout restart deploy nginx -n istio-system
+# kubectl rollout restart deploy nginx -n tools
 
 rm -rf secret-istio.yaml secret-source-apps.yaml
 
+echo "DONE"
